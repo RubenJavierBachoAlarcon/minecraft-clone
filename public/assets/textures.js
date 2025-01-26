a6087dc7d1929df61e4e7db92dfb9c5cb576ead0
Ruben Javier Bacho Alarcon <rjbachoa01@educastillalamancha.es>
2025-01-26 21:00:26 +0100

    Add texture assets and loading functionality; streamline Vite config

---
+++ public/assets/textures.js
@@ -0,0 +1,31 @@
+import { TextureLoader, RepeatWrapping, NearestFilter } from 'three'
+import endstone_brick_img from '../assets/endstone_brick.jpg'
+import purpur_block_img from '../assets/purpur_block.jpg'
+import endstone_img from '../assets/endstone.jpg'
+import chorus_flower_img from '../assets/chorus_flower.jpg'
+import chorus_plant_img from '../assets/chorus_plant.jpg'
+import obsidian_img from '../assets/obsidian.jpg'
+
+const loadTexture = (imagepPath) => {
+  const texture = new TextureLoader().load(imagepPath)
+  texture.magFilter = NearestFilter
+  return texture
+}
+
+const groundTexture = new TextureLoader().load(endstone_img)
+groundTexture.wrapS = groundTexture.wrapT = RepeatWrapping
+groundTexture.magFilter = NearestFilter
+groundTexture.repeat.set(100, 100)
+
+const textures = {
+  endstone_brick: loadTexture(endstone_brick_img),
+  purpur_block: loadTexture(purpur_block_img),
+  endstone: loadTexture(endstone_img),
+  chorus_flower: loadTexture(chorus_flower_img),
+  chorus_plant: loadTexture(chorus_plant_img),
+  obsidian: loadTexture(obsidian_img),
+}
+
+const endstone_brick = loadTexture(endstone_brick_img)
+
+export { groundTexture, textures, endstone_brick }
